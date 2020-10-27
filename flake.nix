67ccdd5c7426186463d5c0bfde5828f1585df40d
Shay Bergmann <sbergmann@kreisys.co>
2020-10-27 10:28:54 -0400

    Add hydra jobs

--- flake.nix
+++ flake.nix
@@ -4,12 +4,20 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake rec {
-      inherit self nixpkgs;
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+    let
       name = "mantis-explorer";
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
       overlay = final: prev: {
-        ${name}.defaultPackage = final.callPackage ./package.nix {};
+        ${name}.defaultPackage = final.callPackage ./package.nix { };
       };
+
+      simpleFlake = flake-utils.lib.simpleFlake
+        {
+          inherit name systems overlay self nixpkgs;
+        };
+    in
+    simpleFlake // {
+      hydraJobs = self.packages;
+      inherit overlay;
     };
 }
