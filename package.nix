3d8b13a5e6e761d7978c7b467607ab0fe352a7a8
Michael Fellinger <michael.fellinger@iohk.io>
2020-12-22 14:55:33 +0100

    Parametise Mantis VMs

--- package.nix
+++ package.nix
@@ -6,10 +6,16 @@ mkYarnPackage {
   WEB3_PROVIDER = "/rpc/node";
   BABEL_ENV = "development";
   NODE_ENV = "development";
+  MANTIS_VM = "VM_Name";
 
   doCheck    = true;
   checkPhase = "yarn test --coverage --ci";
   distPhase  = "true";
+
+  patchPhase = ''
+    substituteInPlace public/index.html --replace "{process.env.MANTIS_VM}" "$MANTIS_VM"
+    substituteInPlace src/components/layout/Header.js --replace "{process.env.MANTIS_VM}" "$MANTIS_VM"
+  '';
 
   buildPhase = ''
     export HOME="$NIX_BUILD_TOP"
