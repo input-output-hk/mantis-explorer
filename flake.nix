4b545d656ddc86e02454a97edca0a9d3bc71ac9c
Shay Bergmann <shay.bergmann@iohk.io>
2021-04-16 12:26:22 -0400

    Use yarn2nix pure-nix fork

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "Mantis Explorer";
 
   inputs.utils.url = "github:kreisys/flake-utils";
+  inputs.yarn2nix.url = "github:input-output-hk/yarn2nix/pure-nix";
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, yarn2nix, utils }:
     utils.lib.simpleFlake
       {
         inherit nixpkgs;
         name = "mantis-explorer";
         systems = [ "x86_64-linux" "x86_64-darwin" ];
+        preOverlays = [ yarn2nix ];
         overlay = final: prev: {
           mantis-explorer = final.callPackage ./package.nix { };
         };
